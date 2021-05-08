dd7eb1e3813284e534ad8214f14935edadbe2d8d
dmitrii <dmitry.esin@gmail.com>
2021-05-08 19:45:00 +0300

    refactor

--- import-service/handler.ts
+++ import-service/handler.ts
@@ -1,11 +1,5 @@
-// import { PostgresProductService } from "src/services/postgres-memory-product-service";
-// import { createProduct } from "@functions/createProduct";
 import { importProductsFile as importProductsFileFn } from "@functions/importProductsFile";
 import { importFileParser as importFileParserFn } from "@functions/importFileParser";
 
 export const importProductsFile = importProductsFileFn;
 export const importFileParser = importFileParserFn;
-
-// const productService = new PostgresProductService(databaseClient);
-
-// export const connectedCreateProduct = createProduct(productService);
