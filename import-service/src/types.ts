5483a4fea010d8146c5f36d417b6e161b65308fb
dmitrii <dmitry.esin@gmail.com>
2021-05-11 13:46:57 +0300

    refactor

--- import-service/src/types.ts
+++ import-service/src/types.ts
@@ -10,31 +10,3 @@ export type ResponseType = {
   headers: Object;
   body: Object;
 };
-
-// export interface Product {
-//   id: string;
-//   count: number;
-//   description: string;
-//   date: string;
-//   location: string;
-//   price: number;
-//   title: string;
-//   image: string;
-// }
-
-// export interface ProductServiceInterface {
-//   getAllProducts: () => Promise<Product[]>;
-//   getProductById: (id: string) => Promise<Product>;
-//   create: (
-//     product: Pick<
-//       Product,
-//       | "count"
-//       | "description"
-//       | "date"
-//       | "location"
-//       | "price"
-//       | "title"
-//       | "image"
-//     >
-//   ) => Promise<Product>;
-// }
